a7d961a2d75eb734caecf3ec9ba8b07c4b47936a
🚀 Rahul Kumar | Full Stack Developer <rahulkumar8340527941@gmail.com>
2025-04-10 08:16:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger--slider-r');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  menu.classList.toggle('active');
+});
+
+
+// Typing Text code
+
+const typed = new Typed('.multiple', {
+    strings: ['Physical Fitness', 'Weight Gain','Strength Training','Fat Loss','Weight Lifting','Running/Cardio'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backdelay: 1000,
+    loop: true,
+  });
